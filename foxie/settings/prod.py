d96bc6286e5343b863c7c2be6bc3ec7e2e3d19c0
James Pulec <jpulec@gmail.com>
2013-09-12 13:09:11 -0500

    Added Procfile

--- foxie/settings/prod.py
+++ foxie/settings/prod.py
@@ -1,1 +1,13 @@
 from common import *
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+STATIC_URL = '/static/'
